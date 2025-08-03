556c8ce8e034b5584c1e3cf07625575612d02e9f
Phdima <dmitrytimonin4@gmail.com>
2025-08-04 04:22:44 +0700

    добавил тему, начал дорабатывать UX UI , сделал его на экране тренажера

--- app/src/main/java/com/example/solverpoker/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/solverpoker/presentation/theme/Theme.kt
@@ -12,32 +12,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Grey,
+    inversePrimary = LightGrey,
+    secondary = Green,
+    onPrimary = White,
+    background = Black,
+    onTertiary = DarkGrey,
+    primaryContainer = Blue,
+    secondaryContainer = Orange,
+    surfaceContainer = anotherGray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Grey,
+    inversePrimary = LightGrey,
+    secondary = Green,
+    onPrimary = White,
+    background = Black,
+    onTertiary = DarkGrey,
+    primaryContainer = Blue,
+    secondaryContainer = Orange,
+    surfaceContainer = anotherGray
 )
+
 
 @Composable
 fun SolverPokerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
